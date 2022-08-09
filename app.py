56ae03c5b5c2429a8ca178c8fba8272bcb0b64c2
Joseph Weidinger <jsphweid@gmail.com>
2022-08-09 16:44:11 -0500

    remove debug

--- app.py
+++ app.py
@@ -305,4 +305,4 @@ def transcribe_anythin():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 1234))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
